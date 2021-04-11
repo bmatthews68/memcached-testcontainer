7759d49957e3d65c73a63be6c2a4b73ea6ab8c56
Brian Matthews <brian@btmatthews.com>
2021-04-12 01:41:19 +0300

    Initial commit

---
+++ src/main/java/com/btmatthews/testcontainers/memcached/MemcachedContainer.java
@@ -0,0 +1,41 @@
+package com.btmatthews.testcontainers.memcached;
+
+import net.spy.memcached.MemcachedClient;
+import net.spy.memcached.MemcachedClientIF;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.HostPortWaitStrategy;
+import org.testcontainers.utility.DockerImageName;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+
+public class MemcachedContainer extends GenericContainer<MemcachedContainer> {
+
+    private static final DockerImageName DEFAULT_IMAGE_NAME = DockerImageName.parse("memcached");
+    private static final String DEFAULT_TAG = "1.6.9";
+    private static final int MEMCACHED_PORT = 11211;
+
+    @Deprecated
+    public MemcachedContainer() {
+        this(DEFAULT_IMAGE_NAME.withTag(DEFAULT_TAG));
+    }
+
+    public MemcachedContainer(final String dockerImageName) {
+        this(DockerImageName.parse(dockerImageName));
+    }
+
+    public MemcachedContainer(final DockerImageName dockerImageName) {
+        super(dockerImageName);
+
+        dockerImageName.assertCompatibleWith(DEFAULT_IMAGE_NAME);
+
+        addExposedPorts(MEMCACHED_PORT);
+        setWaitStrategy(new HostPortWaitStrategy());
+    }
+
+    public MemcachedClientIF getClient() throws IOException {
+        final InetSocketAddress address = new InetSocketAddress(InetAddress.getLocalHost(), getMappedPort(MEMCACHED_PORT));
+        return new MemcachedClient(address);
+    }
+}
---
+++ src/test/java/com/btmatthews/testcontainers/memcached/TestMemcachedContainer.java
@@ -0,0 +1,24 @@
+package com.btmatthews.testcontainers.memcached;
+
+import net.spy.memcached.MemcachedClientIF;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Testcontainers
+class TestMemcachedContainer {
+
+    @Container
+    private static final MemcachedContainer memcachedContainer = new MemcachedContainer();
+
+    @Test
+    void client() throws IOException {
+        final MemcachedClientIF client = memcachedContainer.getClient();
+        client.add("key", 0, "value");
+        assertThat(client.get("key")).isEqualTo("value");
+    }
+}
